39f7bc1e5273f2775cfa96fed391fcc2e7764ff1
Harold Abr <abreuh519@gmail.com>
2024-07-30 16:06:58 -0400

    Update unver.py

--- unver.py
+++ unver.py
@@ -1,8 +1,9 @@
-nombre = "harold "
+nombre = "harld "
 numero = 12 
 venta = 90
 
 for i in range (numero):
     print ("el numero es doce")
-
+if nombre is not "harold ":
+    print("el nombre no es harold")
 
